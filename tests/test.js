2cf9abbcbda0140feed7b1dac2d412e802590b21
Stephen Devine <stephend@pensaredevelopment.com>
2016-01-12 14:37:33 -0800

    use an associative array to get the most common string in the array of input strings

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,10 @@
 var test = require('tape');
 var systemUnderTest = require('../lib/file.js');
 
-test('System under test Should export a function', assert => {
-  assert.plan(1);
-  const actual = typeof systemUnderTest.foo();
-  const expected = 'function';
-
-  assert.equal(actual, expected, 'foo() should return a function.');
+test('mostCommon() should return the most common string in the input array', assert => {
+  assert.equal(systemUnderTest.mostCommon(["b", "a", "a"]), "a", 'a should be the most common string.');
+  assert.equal(systemUnderTest.mostCommon(["b", "b", "a"]), "b", 'b should be the most common string.');
+  assert.equal(systemUnderTest.mostCommon(["c", "d", "e"]), "c", 'a should be the most common string.');
+  assert.equal(systemUnderTest.mostCommon(["f", "f", "f"]), "f", 'a should be the most common string.');
+  assert.end();
 })
